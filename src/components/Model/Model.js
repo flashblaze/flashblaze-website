a3d35a91e24fb4a7789da188bf3c3e17cbc3b700
Neeraj Lagwankar <neerajlagwankar@gmail.com>
2019-12-23 19:32:39 +0530

    Displaying a static 3D cube 💥

--- src/components/Model/Model.js
+++ src/components/Model/Model.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Engine, Scene } from 'react-babylonjs';
-import { Vector3, Color4 } from '@babylonjs/core';
+import { Vector3, Color4, Mesh } from '@babylonjs/core';
 
 const Model = () => (
   <Engine canvasId="sample-canvas">
     <Scene clearColor={new Color4(0, 0, 0, 0)}>
-      <freeCamera
+      <targetCamera
         name="camera1"
         position={new Vector3(0, 1, -10)}
         target={Vector3.Zero()}
@@ -15,10 +15,12 @@ const Model = () => (
         intensity={0.7}
         direction={Vector3.Up()}
       />
-      <sphere
-        name="sphere1"
-        diameter={3}
-        segments={64}
+      <box
+        name="box"
+        height={2}
+        width={2}
+        depth={2}
+        rotation={new Vector3(90, 90, 180)}
         position={new Vector3(0, 1, 0)}
       />
     </Scene>
