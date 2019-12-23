b964e890bd9c773b7e1a577d7f54affffa47148b
Neeraj Lagwankar <neerajlagwankar@gmail.com>
2019-12-23 20:27:32 +0530

    Removed babylonjs
    I'll be using react-three-fiber along with threejs which works well with SSR. Since I'm using Gatsby, I don't have access to window object and thus could not use react-babylonjs which is solved by react-three-fiber.

--- src/components/Model/Model.js
+++ src/components/Model/Model.js
@@ -1,30 +1,11 @@
 import React from 'react';
-import { Engine, Scene } from 'react-babylonjs';
-import { Vector3, Color4, Mesh } from '@babylonjs/core';
 
-const Model = () => (
-  <Engine canvasId="sample-canvas">
-    <Scene clearColor={new Color4(0, 0, 0, 0)}>
-      <targetCamera
-        name="camera1"
-        position={new Vector3(0, 1, -10)}
-        target={Vector3.Zero()}
-      />
-      <hemisphericLight
-        name="light1"
-        intensity={0.7}
-        direction={Vector3.Up()}
-      />
-      <box
-        name="box"
-        height={2}
-        width={2}
-        depth={2}
-        rotation={new Vector3(90, 90, 180)}
-        position={new Vector3(0, 1, 0)}
-      />
-    </Scene>
-  </Engine>
-);
+const Model = () => {
+  return (
+    <div>
+      <p>Model will go here</p>
+    </div>
+  );
+};
 
 export default Model;
