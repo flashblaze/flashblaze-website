968e09cb211ad2c1cdd950a1d98278673824a47b
Neeraj Lagwankar <neerajlagwankar@gmail.com>
2019-12-25 09:39:36 +0530

    Google site verification

--- src/components/Head.js
+++ src/components/Head.js
@@ -14,6 +14,10 @@ const Head = () => {
         />
         <html lang="en" amp />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="google-site-verification"
+          content="bLKH-xkzCKP2YUvhotEuL0NvIRQmHI_DnFkkqRS5sJA"
+        />
       </Helmet>
     </div>
   );
