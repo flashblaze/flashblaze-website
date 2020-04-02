098e9a030b754c633ba4d30491414a9d28ac3b32
FlashBlaze <neerajlagwankar@gmail.com>
2020-04-02 14:11:10 +0530

    Added firebase

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,2 @@
 require('prismjs/themes/prism-tomorrow.css');
+require('firebase/database');
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,6 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
 module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -72,6 +75,21 @@ module.exports = {
             resolve: `gatsby-remark-prismjs`,
           },
         ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-firebase`,
+      options: {
+        credentials: {
+          apiKey: process.env.API_KEY,
+          authDomain: process.env.AUTH_DOMAIN,
+          databaseURL: process.env.DATABASE_URL,
+          projectId: process.env.PROJECT_ID,
+          storageBucket: process.env.STORAGE_BUCKET,
+          messagingSenderId: process.env.MESSAGING_SENDER_ID,
+          appId: process.env.APP_ID,
+          measurementId: process.env.MEASUREMENT_ID,
+        },
       },
     },
   ],
